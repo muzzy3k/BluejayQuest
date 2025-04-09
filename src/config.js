e6cba27e01e67c1efbb83f186150e35a533391bc
muzzy3k <islamm@etwon.edu>
2025-04-09 00:02:30 -0400

    update things

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
